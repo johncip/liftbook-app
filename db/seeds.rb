96f533d45b6a32adcc668ce0926cf4ac1f6f71a7
John Cipriano <johnmikecip@gmail.com>
2017-05-14 15:29:07 -0700

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+def create_user_content(user)
+  bp = Lift.create(user: user, name: 'Bench Press')
+  Nickname.create(lift: bp, name: 'bp')
+  Entry.create(lift: bp, sets: 5, reps: 5, weight: 135, units: :lb)
+end
+
+john = User.create(name: 'John', email: 'john@liftbook.io')
+create_user_content(john)
