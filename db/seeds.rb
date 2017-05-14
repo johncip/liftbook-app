502f53e57b2d792ca694c615c0f78d0d991cfe50
John Cipriano <johnmikecip@gmail.com>
2017-05-14 15:36:37 -0700

    rubocop autocorrect and clean up

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 def create_user_content(user)
   bp = Lift.create(user: user, name: 'Bench Press')
   Nickname.create(lift: bp, name: 'bp')
