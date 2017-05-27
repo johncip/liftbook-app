1aaefe352790f6efd2224841fb833439d8e7fe56
John Cipriano <johnmikecip@gmail.com>
2017-05-27 16:11:56 -0700

    update models

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,8 @@
 
 # A Liftbook user.
 class User < ApplicationRecord
-  # has_many :workouts
-  # has_many :entries, through: :workouts
+  has_many :workouts
+  has_many :entries, through: :workouts
 
   validates :email, uniqueness: true
 
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -11,5 +11,6 @@
 
 # TODO: Workouts need some kind of user-friendly unique identifier.
 class Workout < ApplicationRecord
+  belongs_to :user
   has_many :entries
 end
