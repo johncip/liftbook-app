875672b848e864ac29c219ed6ca73acb7c2b8ed0
John Cipriano <johnmikecip@gmail.com>
2017-05-27 15:05:15 -0700

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
 def create_user_content(user)
-  bp = Lift.create(user: user, name: 'Bench Press')
-  Nickname.create(lift: bp, name: 'bp')
-  Entry.create(lift: bp, sets: 5, reps: 5, weight: 135, units: :lb)
+  bench = Lift.create!(user: user, name: 'Bench Press')
+  workout = Workout.create!(date: Date.new(2017, 5, 13))
+  workout.entries.create!(lift: bench, sets: 5, reps: 5, weight: 135, units: :lb)
+  Nickname.create!(lift: bench, name: 'bp')
 end
 
-john = User.create(name: 'John', email: 'john@liftbook.io')
+john = User.create!(name: 'John', email: 'john@liftbook.io')
 create_user_content(john)
