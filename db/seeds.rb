3ab65fb063cef507575077e0a4befffecbd21aab
John Cipriano <johnmikecip@gmail.com>
2017-05-27 20:10:59 -0700

    update seeds & change formatting

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,29 @@
 def create_user_content(user)
-  bench = Lift.create!(user: user, name: 'Bench Press')
-  bench.nicknames.create!(lift: bench, name: 'bp')
-
-  workout = Workout.create!(user: user, date: Date.new(2017, 5, 13))
-  workout.entries.create!(lift: bench, sets: 5, reps: 5, weight: 135, units: :lb)
+  bench = Lift.create!(
+    user: user,
+    name: 'Bench Press'
+  )
+  bench.nicknames.create!(
+    lift: bench,
+    name: 'bp'
+  )
+  workout = Workout.create!(
+    user: user,
+    name: 'Chest Day',
+    date: Date.new(2017, 5, 13)
+  )
+  workout.entries.create!(
+    lift: bench,
+    sets: 5,
+    reps: 5,
+    weight: 135,
+    units: :lb
+  )
 end
 
-john = User.create!(name: 'John', email: 'john@liftbook.io')
+
+john = User.create!(
+  name: 'John',
+  email: 'john@liftbook.io'
+)
 create_user_content(john)
