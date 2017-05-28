20383b7bf98b2b5d717d0f6d312ac33b042791fe
John Cipriano <johnmikecip@gmail.com>
2017-05-28 02:13:39 -0700

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :workouts, only: [:show] do
+    resources :entries, only: [:create], shallow: true do
+      post :create_from_nickname
+    end
+  end
 end
