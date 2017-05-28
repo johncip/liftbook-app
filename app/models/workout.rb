4be045b9810dc6b3c56e48c6b1765305f6f20b9e
John Cipriano <johnmikecip@gmail.com>
2017-05-27 20:10:51 -0700

    annotate

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -8,6 +8,7 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  user_id    :integer          not null
+#  name       :string           default("New Workout"), not null
 #
 
 class Workout < ApplicationRecord
